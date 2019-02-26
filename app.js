89d55e48de722289c4f5cc1190acef329f225bca
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-02-25 17:09:58 -0800

    Added new endpoints and index page

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.set('json spaces', 2)
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -18,11 +18,69 @@ router.get('/', function(req, res, next) {
   	res.render('index', { title: 'Pablo' });
 });
 
+//GET requests
 router.get('/users', function(req, res, next) {
 	knex.select().table('users')
 	.then(function(users){
-		res.send(JSON.stringify(users))
-	});
+		res.json(users)
+	})
+});
+
+router.get('/drivers', function(req, res, next) {
+    knex.select().table('drivers')
+    .then(function(drivers){
+        res.json(drivers)
+    });
+});
+
+router.get('/rides', function(req, res, next) {
+    knex.select().table('rides')
+    .then(function(rides){
+        res.json(rides)
+    });
+});
+
+router.get('/locations', function(req, res, next) {
+    knex.select().table('locations')
+    .then(function(locations){
+        res.json(locations)
+    });
+});
+
+//POST requests
+router.post('/user', function(req, res, next) {
+    console.log(JSON.stringify(req.body));
+    knex('users').insert(req.body)
+    .then(res.send("Success"))
+    .catch(function(err) {
+        console.log(err.stack);
+    })
+});
+
+router.post('/driver', function(req, res, next) {
+    console.log(JSON.stringify(req.body));
+    knex('drivers').insert(req.body)
+    .catch(function(err) {
+        console.log(err.stack);
+    })
+});
+
+router.post('/ride', function(req, res, next) {
+    console.log(JSON.stringify(req.body));
+    knex('rides').insert(req.body)
+    .then(res.send("Success"))
+    .catch(function(err) {
+        console.log(err.stack);
+    })
+});
+
+router.post('/location', function(req, res, next) {
+    console.log(JSON.stringify(req.body));
+    knex('locations').insert(req.body)
+    .then(res.send("Success"))
+    .catch(function(err) {
+        console.log(err.stack);
+    })
 });
 
 module.exports = router;
